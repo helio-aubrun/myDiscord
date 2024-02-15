03cea8a191a61612f6c35d481ef59d7ed86780ac
lucas-lubin <146708354+lucas-lubin@users.noreply.github.com>
2024-02-15 13:42:02 +0100

    ADD read_user
    read user + update for the code more clear.

--- User.py
+++ User.py
@@ -5,12 +5,17 @@ class user:
     def __init__(self,db):
        self.db= db
 
-    
-    def add_user ( self, nom, prenom, pseudo, email, mdp):
-         query = "INSERT INTO user (nom, prenom, pseudo, email, mdp, id_channel, admin) VALUES (?,?,?,?,?,?)"
-         values = (nom, pseudo, email, mdp, [1], 0)
-         self.db.execute_query(query, values)
+def add_user(self, nom, prenom, pseudo, email, mdp):
+    query = "INSERT INTO user (nom, prenom, pseudo, email, mdp, id_channel, admin) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    values = (nom, prenom, pseudo, email, mdp, 1, 0)
+    self.db.execute_query(query, values)
 
-    def delete_user(self, nom, pseudo, email, mdp):        
-        query = "DELETE FROM category WHERE id = %s"
-        self.db.execute_query(query, (nom, pseudo, email, mdp))
+def delete_user(self, nom, pseudo, email, mdp):
+    query = "DELETE FROM user WHERE nom = %s AND pseudo = %s AND email = %s AND mdp = %s"
+    self.db.execute_query(query, (nom, pseudo, email, mdp))
+
+def read_user(self, nom, prenom, pseudo, email, mdp, admin, id_channel):
+    query = "SELECT * FROM user WHERE nom = %s AND prenom = %s AND pseudo = %s AND email = %s AND mdp = %s AND admin = %s AND id_channel = %s"
+    values = (nom, prenom, pseudo, email, mdp, admin, id_channel)
+    results = self.db.execute_query(query, values)
+    return results
