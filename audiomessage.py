829ae86a0281b247e011d92a24a6b9c127c81def
helio-aubrun <helio.aubrun@laplateforme.io>
2024-02-20 10:01:44 +0100

    Update audiomessage.py
    add commentary

--- audiomessage.py
+++ audiomessage.py
@@ -8,7 +8,7 @@ class AudioMessage:
     def create_audio(self, id_channel, id_user, duree_enregistrement=5, frequence_echantillonnage=44100, format_audio=pyaudio.paInt16, canaux=2):
         CHUNK = 1024
         p = pyaudio.PyAudio()
-
+        #record
         stream = p.open(format=format_audio,
                         channels=canaux,
                         rate=frequence_echantillonnage,
@@ -28,7 +28,7 @@ class AudioMessage:
         stream.stop_stream()
         stream.close()
         p.terminate()
-
+        #end record and save audio file
         audio_data = b''.join(frames)
         nom_fichier = f"message_audio_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.wav"
         taille_fichier = len(audio_data)
@@ -36,7 +36,7 @@ class AudioMessage:
         time = datetime.now()
 
         try:
-            # Insertion des données audio dans la base de données
+            #audio data in database
             sql = "INSERT INTO message_audio (id_channel, id_user, time, audio_data, nom_fichier, taille_fichier, type_mime) VALUES (%s, %s, %s, %s, %s, %s, %s)"
             self.db.execute_query(sql, (id_channel, id_user, time, audio_data, nom_fichier, taille_fichier, type_mime))
             print("Données audio enregistrées dans la base de données avec succès.")
