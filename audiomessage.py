3fd72cb8578f42bbfff4cbda5563879cad5f815d
helio-aubrun <helio.aubrun@laplateforme.io>
2024-02-20 10:44:10 +0100

    add delete_audio and play_audio
    play_audio still in progress ( have to download ffmpeg

--- audiomessage.py
+++ audiomessage.py
@@ -1,5 +1,8 @@
+from database import Database
 import pyaudio
 from datetime import datetime
+from pydub import AudioSegment
+import io
 
 class AudioMessage:
     def __init__(self, db):
@@ -15,7 +18,7 @@ class AudioMessage:
                         input=True,
                         frames_per_buffer=CHUNK)
 
-        print("Enregistrement en cours...")
+        print("recording...")
 
         frames = []
 
@@ -23,7 +26,7 @@ class AudioMessage:
             data = stream.read(CHUNK)
             frames.append(data)
 
-        print("Enregistrement terminé.")
+        print("recording is over.")
 
         stream.stop_stream()
         stream.close()
@@ -39,6 +42,33 @@ class AudioMessage:
             #audio data in database
             sql = "INSERT INTO message_audio (id_channel, id_user, time, audio_data, nom_fichier, taille_fichier, type_mime) VALUES (%s, %s, %s, %s, %s, %s, %s)"
             self.db.execute_query(sql, (id_channel, id_user, time, audio_data, nom_fichier, taille_fichier, type_mime))
-            print("Données audio enregistrées dans la base de données avec succès.")
+            print("Audio data saved in the database successfully.")
         except Exception as e:
-            print(f"Erreur lors de l'enregistrement des données audio : {e}")
+            print(f"Error recording audio data : {e}")
+
+    def delete_audio(self, id_audio):
+        query = "DELETE FROM message_audio WHERE id = %s"
+        self.db.execute_query(query, (id_audio,))
+
+        
+    def play_audio(self, id_audio):
+        try:
+            # Recover audio data from database
+            sql = "SELECT audio_data FROM message_audio WHERE id = %s"
+            result = self.db.fetch_all(sql, (id_audio,))
+            audio_data = result[0][0]  # The query returns a tuple with a single element (audio_data)
+
+            # Convert audio data to AudioSegment
+            audio_segment = AudioSegment.from_file(io.BytesIO(audio_data))
+
+            #audio playback
+            audio_segment.play()
+
+        except Exception as e:
+            print(f"Error playing audio from database : {e}")
+
+if __name__ == "__main__":
+    db = Database(host="localhost", user="root", password="310104", database="myDiscord")
+    test = AudioMessage(db)
+    """test.create_audio(1,1)"""
+    test.play_audio(1)
