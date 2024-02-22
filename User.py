7b0b3428e17ca06103553a5c938b5492b01712ca
lucas-lubin <146708354+lucas-lubin@users.noreply.github.com>
2024-02-22 14:54:13 +0100

    UPDATE OF THE MONTH
    ADD CHECK MAIL AND USER , CHANGING THE LINGE FOR DELETE_USER and READ_USER YOU CAN ONLY USE THE ID FOR MORE RAPIDITY AND EFFICACITY.

--- User.py
+++ User.py
@@ -1,29 +1,59 @@
 from database import Database
 
+class User:
 
 
-class user:
     def __init__(self,db):
        self.db= db
 
 
+    # ... autres méthodes de la classe ...
+
     def add_user(self, nom, prenom, pseudo, email, mdp):
-        query = "INSERT INTO user (nom, prenom, pseudo, email, mdp, id_channel, admin) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        values = (nom, prenom, pseudo, email, mdp,   1,   0)  # Changé [1] en   1
-        self.db.execute_query(query, values)
+        if self.check_email(email):
+            print('Cet e-mail existe déjà!')
+            return  # Arrête l'exécution si l'email existe déjà
+        elif self.check_pseudo(pseudo):
+            print('Ce pseudo existe déjà!')
+            return  # Arrête l'exécution si le pseudo existe déjà
+        else:
+            query = "INSERT INTO user (nom, prenom, pseudo, email, mdp, id_channel, admin) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            values = (nom, prenom, pseudo, email, mdp,  1,  0)
+            try:
+                self.db.execute_query(query, values)
+                print('Utilisateur ajouté avec succès.')
+            except Exception as e:
+                print(f'Erreur lors de l\'ajout de l\'utilisateur: {e}')
 
-    def delete_user(self, nom, pseudo, email, mdp):
-        query = "DELETE FROM user WHERE nom = %s AND pseudo = %s AND email = %s AND mdp = %s"
-        self.db.execute_query(query, (nom, pseudo, email, mdp))
-
-    def read_user(self, nom, prenom, pseudo, email, mdp, admin, id_channel):
-        query = "SELECT * FROM user WHERE nom = %s AND prenom = %s AND pseudo = %s AND email = %s AND mdp = %s AND admin = %s AND id_channel = %s"
-        values = (nom, prenom, pseudo, email, mdp, admin, id_channel)
-        results = self.db.execute_query(query, values)
+    def delete_user(self, id):
+        query = "DELETE FROM user WHERE id = %s"
+        results = self.db.execute_query(query, (id,)) 
         return results
 
-   
+    def read_user(self, id):
+        query = "SELECT * FROM user WHERE id = %s"
+        results = self.db.fetch_all(query, (id,))  
+        return results
+
+    def check_email(self, email):
+        query = "SELECT email FROM user WHERE email = %s"
+        result = self.db.fetch_all(query, (email,))
+        return result
+            
+    def check_pseudo(self, pseudo):
+        query = "SELECT pseudo FROM user WHERE pseudo = %s"
+        result = self.db.fetch_all(query, (pseudo,))
+        return result 
+    
 if __name__ == "__main__":
     db = Database(host="localhost", user="root", password="AstaxYuno972.", database="ma_base")
-    test = user(db)
-    test.delete_user("nom","pseudo", "email@example.com", "password")
+    test = User(db)
+    test.add_user("lucas","lubin","amir", "sdfev.com", "pdksdicns")
+    
+    id_to_delete = 12
+    result = test.delete_user(id_to_delete)
+    print(result)
+
+    id_to_read = 2
+    result = test.read_user(id_to_read)
+    print(result)
