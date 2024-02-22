58b96c9064bcf93c2030bf73cf3e0434e168a5c9
lucas-lubin <146708354+lucas-lubin@users.noreply.github.com>
2024-02-22 15:40:49 +0100

    1.4.2
    ADD READ_ALL_USER +ENGLISH

--- User.py
+++ User.py
@@ -7,21 +7,20 @@ class User:
        self.db= db
 
 
-    # ... autres méthodes de la classe ...
 
     def add_user(self, nom, prenom, pseudo, email, mdp):
         if self.check_email(email):
-            print('Cet e-mail existe déjà!')
-            return  # Arrête l'exécution si l'email existe déjà
+            print('E-MAIL EXIST')
+            return  # Stop execution if the email already exists
         elif self.check_pseudo(pseudo):
-            print('Ce pseudo existe déjà!')
-            return  # Arrête l'exécution si le pseudo existe déjà
+            print('PSEUDO EXIST')
+            return  # Stop execution if the username already exists
         else:
             query = "INSERT INTO user (nom, prenom, pseudo, email, mdp, id_channel, admin) VALUES (%s, %s, %s, %s, %s, %s, %s)"
             values = (nom, prenom, pseudo, email, mdp,  1,  0)
             try:
                 self.db.execute_query(query, values)
-                print('Utilisateur ajouté avec succès.')
+                print('USER ADD .')
             except Exception as e:
                 print(f'Erreur lors de l\'ajout de l\'utilisateur: {e}')
 
@@ -30,10 +29,22 @@ class User:
         results = self.db.execute_query(query, (id,)) 
         return results
 
-    def read_user(self, id):
+    def read_user_id(self, id):
         query = "SELECT * FROM user WHERE id = %s"
         results = self.db.fetch_all(query, (id,))  
         return results
+    
+    def read_user_pseudo(self, pseudo):
+        query = "SELECT * FROM user WHERE pseudo = %s"
+        results = self.db.fetch_all(query, (pseudo,))  
+        return results
+
+
+    def read_all_user(self):
+        query = "SELECT * FROM USER"
+        result = self.db.fetch_all(query)
+        return result
+    
 
     def check_email(self, email):
         query = "SELECT email FROM user WHERE email = %s"
@@ -43,17 +54,18 @@ class User:
     def check_pseudo(self, pseudo):
         query = "SELECT pseudo FROM user WHERE pseudo = %s"
         result = self.db.fetch_all(query, (pseudo,))
-        return result 
-    
+        return result
+
 if __name__ == "__main__":
     db = Database(host="localhost", user="root", password="AstaxYuno972.", database="ma_base")
     test = User(db)
     test.add_user("lucas","lubin","amir", "sdfev.com", "pdksdicns")
-    
+
     id_to_delete = 12
     result = test.delete_user(id_to_delete)
     print(result)
 
     id_to_read = 2
-    result = test.read_user(id_to_read)
+    result = test.read_user_pseudo(id_to_read)
     print(result)
+
