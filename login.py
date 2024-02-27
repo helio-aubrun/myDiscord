d7762583ad234e468604c31aec835ba498cb3241
ali-bousrira <146709104+ali-bousrira@users.noreply.github.com>
2024-02-27 14:09:32 +0100

    adding the fonction to go to room frames

--- login.py
+++ login.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import messagebox
 from acount_creation import Acount_creation
 from room import Room
+from User import User
+from database import Database
 
 class Login(tk.Frame):
     def __init__(self, parent, controller):
@@ -11,12 +13,11 @@ class Login(tk.Frame):
         self.configure(padx=20, pady=20)  # Increase padding around the frame
 
         # Similarly, set the background color for the label using a hexadecimal color code
-        label_bg_color = "#1E1E1E"  # This is the hexadecimal equivalent of RGB(30,  30,  30)
         label = tk.Label(self, text="Login", font=("Helvetica",   20), fg="#f2f3f5", bg="#313338")  # Increase font size and set background color
         label.pack(pady=10)
         
         # The rest of your code remains the same
-        username_label = tk.Label(self, text="Email", font=("Helvetica",  14), fg="#f2f3f5", bg="#313338")  # Increase font size and set background color
+        username_label = tk.Label(self, text="Pseudo", font=("Helvetica",  14), fg="#f2f3f5", bg="#313338")  # Increase font size and set background color
         username_label.pack()
 
         self.username_entry = tk.Entry(self, font=("Helvetica",  14))  # Increase font size
@@ -39,8 +40,9 @@ class Login(tk.Frame):
         password = self.password_entry.get()
         
         # Here you would typically check the credentials against a database or other storage
-        if username == "a" and password == "z":
+        test_user = User (Database(host="localhost", user="root", password="2879", database="mydiscord"))
+        user_info = test_user.read_user_pseudo (username)
+        if password == user_info [0][4] :
             messagebox.showinfo("Login Successful", "Welcome!")
-            controller.show_frame(Acount_creation)
-        else:
-            messagebox.showerror("Login Failed", "Invalid username or password.")
+            controller.show_frame(Room)
+
